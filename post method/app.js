99a76dfc30120ab50dacb364726ac6d3a4e9b2a7
unknown <sabir786akhtarkhan@gmail.com>
2024-03-29 23:50:45 +0530

    post-method

---
+++ post method/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+//middleware
+app.use(express.json());
+//express.json()  will parse the json data
+
+
+app.post('',(req,res)=>{
+    //req.body will help you to access data from frontend
+    console.log(req.body.name);
+    console.log(req.body.age);
+    res.send("welcome to post method");
+})
+
+app.post('/add',(req,res)=>{
+   const num1 = parseInt(req.body.num1);
+   const num2 = parseInt(req.body.num2);
+   const result = num1 + num2;
+   res.send({result:result});
+})
+
+app.post('/sub',(req,res)=>{
+    const num1 = parseInt(req.body.num1);
+    const num2 = parseInt(req.body.num2);
+    const result = num1 - num2;
+    res.send({result:result});
+ })
+
+ app.post('/mult',(req,res)=>{
+    const num1 = parseInt(req.body.num1);
+    const num2 = parseInt(req.body.num2);
+    const result = num1 * num2;
+    res.send({result:result});
+ })
+
+ 
+module.exports = app;
